87a667ed4dfc859581a3dd8309696054c64478a1
manar <menarmms9@gmail.com>
2025-12-04 19:00:00 +0300

    init project

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.itjs to start work ing on your app!</Text>
+      <Text>Open up App.itjs to start wo. rk ing on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
